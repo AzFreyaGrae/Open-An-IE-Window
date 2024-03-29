5a4d4cf40065c9e51c88bb03604beb41d12a2188
AzFreyaGrace <120556473+AzFreyaGrae@users.noreply.github.com>
2024-03-29 11:22:24 +0800

    Add files via upload

---
+++ IE/IE/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace IE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.软件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.作者ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.广告位招租ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 37);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(404, 63);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.菜单ToolStripMenuItem,
+            this.关于ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(428, 32);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.软件ToolStripMenuItem,
+            this.作者ToolStripMenuItem});
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(58, 28);
+            this.关于ToolStripMenuItem.Text = "关于";
+            // 
+            // 软件ToolStripMenuItem
+            // 
+            this.软件ToolStripMenuItem.Name = "软件ToolStripMenuItem";
+            this.软件ToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.软件ToolStripMenuItem.Text = "软件";
+            this.软件ToolStripMenuItem.Click += new System.EventHandler(this.软件ToolStripMenuItem_Click);
+            // 
+            // 作者ToolStripMenuItem
+            // 
+            this.作者ToolStripMenuItem.Name = "作者ToolStripMenuItem";
+            this.作者ToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.作者ToolStripMenuItem.Text = "作者";
+            this.作者ToolStripMenuItem.Click += new System.EventHandler(this.作者ToolStripMenuItem_Click);
+            // 
+            // 菜单ToolStripMenuItem
+            // 
+            this.菜单ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.广告位招租ToolStripMenuItem});
+            this.菜单ToolStripMenuItem.Name = "菜单ToolStripMenuItem";
+            this.菜单ToolStripMenuItem.Size = new System.Drawing.Size(58, 28);
+            this.菜单ToolStripMenuItem.Text = "菜单";
+            // 
+            // 广告位招租ToolStripMenuItem
+            // 
+            this.广告位招租ToolStripMenuItem.Name = "广告位招租ToolStripMenuItem";
+            this.广告位招租ToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.广告位招租ToolStripMenuItem.Text = "广告位招租";
+            this.广告位招租ToolStripMenuItem.Click += new System.EventHandler(this.广告位招租ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 112);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(445, 133);
+            this.Name = "Form1";
+            this.Text = "Open An IE Window";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 菜单ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 广告位招租ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 软件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 作者ToolStripMenuItem;
+    }
+}
+
---
+++ IE/IE/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IE
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadEmbeddedHtml();
+        }
+        
+        private void LoadEmbeddedHtml()
+        {
+            // 获取嵌入资源的命名空间
+            string resourceNamespace = "IE"; // 修改为你的命名空间
+
+            // 构建资源路径
+            string resourcePath = resourceNamespace + ".10.html";
+
+            // 从程序集中读取资源文件内容
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Stream resourceStream = assembly.GetManifestResourceStream(resourcePath);
+
+            if (resourceStream != null)
+            {
+                using (StreamReader reader = new StreamReader(resourceStream))
+                {
+                    string htmlContent = reader.ReadToEnd();
+                    webBrowser1.DocumentText = htmlContent;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Failed to load embedded HTML resource.");
+            }
+        }
+
+        private void 软件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" - 这是偶然间发现的一个新方式！！！ \n - 不知道在其它电脑上怎样？ \n - Ps:新发现！嘿咻!", "Software Message !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void 作者ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" - Ae （2023/03/28） \n - FreyaGrace  \n - Q Group: 767508000", "Author Message !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void 广告位招租ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" - 再这样下去，就要穷的吃土了！！！！", "Advertisement Message !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
